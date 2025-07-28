1b23d56bf2ef130d22f73b64fd872c2ff3821042
Cypher14x <alexcoro14x@gmail.com>
2025-07-28 11:41:43 -0500

    Tipos completados

---
+++ tipos/null-undefined.js
@@ -0,0 +1,5 @@
+"use strict";
+(() => {
+    let nada = undefined; // Error: Type 'undefined' is not assignable to type 'number'.
+    console.log(nada);
+})();
---
+++ tipos/null-undefined.ts
@@ -0,0 +1,9 @@
+(() => {
+
+    let nada:number = undefined; // Error: Type 'undefined' is not assignable to type 'number'.
+
+
+    console.log(nada);
+    
+
+})()
