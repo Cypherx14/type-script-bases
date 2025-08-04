3cef588083bb93ef12e7bc734c2144739dcedc2e
Cypher14x <alexcoro14x@gmail.com>
2025-08-04 09:47:47 -0500

    desestructuring

---
+++ ES6/desestructuracion.ts
@@ -0,0 +1,34 @@
+(()=>{
+    
+    type Avenger = {
+        nick: string;
+        ironman: string;
+        spiderman: string;
+        activo: boolean;
+        poder: number;
+    }
+    
+    const avengers: Avenger = {
+        nick: 'Nick Fury',
+        ironman: 'Tony Stark',
+        spiderman: 'Tom Holland',
+        activo: true,
+        poder: 1500,
+    }
+
+    // Desestructuración de objetos, no importante el orden
+    const { nick, ironman, spiderman, activo, poder } = avengers;
+
+    const printAvenger = ({nick, ironman, spiderman, activo, poder}:Avenger) => {
+        console.log(`Nick: ${nick}`);
+        console.log(`Ironman: ${ironman}`);
+        console.log(`Spiderman: ${spiderman}`);
+        console.log(`Activo: ${activo}`);
+        console.log(`Poder: ${poder}`);
+    }
+
+    
+
+
+
+})()
